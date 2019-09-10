3a4b20479b684bbab7ce1422290d34e9a16a6cfd
Edgar V <evenegas9@ucmerced.edu>
2019-09-10 22:31:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,17 +3,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  word.scan(/un\w*ing/)
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.split(" ").grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   !!text.match(/^[A-Z].*\.$/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.grep(/(\d){10}/)
 end
